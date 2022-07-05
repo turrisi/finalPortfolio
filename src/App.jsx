836370f283e789ae66588fd489dd8d65a24a2929
turrisi <dip.aturrisi@gmail.com>
2022-07-05 16:18:15 -0300

    removed commented stuf

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-// import Portfolio from './components/portfolio/portfolio'
-// import { About } from './components/about/about'
 import './App.css'
 import Landing from './components/landing/landing'
 import NavBar from './components/navbar/navbar'
-// import Yo from './helpers/about'
-// import Academic from './helpers/studies'
-// import Exp from './helpers/experience'
-// import SkilHab from './helpers/skils'
-// import Contact from './helpers/contact'
-// import Project from './helpers/project'
 
 
 function App() {
@@ -17,14 +9,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element ={<Landing/>} />
-        {/* <Route path="/portfolio/:lan" element = {<Portfolio/>} /> */}
         <Route path="/portfolio/:lan" element = {<NavBar/>} />
-        {/* <Route path="/portfolio/:lan/about" element = {<Yo/>} />
-        <Route path="/portfolio/:lan/skills" element = {<SkilHab/>} />
-        <Route path="/portfolio/:lan/academics" element = {<Academic/>} />
-        <Route path="/portfolio/:lan/experience" element = {<Exp/>} />
-        <Route path="/portfolio/:lan/contact" element = {<Contact/>} />
-        <Route path="/portfolio/:lan/proyects" element = {<Project/>} /> */}
       </Routes>
     </div>
   )
